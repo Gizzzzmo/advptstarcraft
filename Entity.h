bae941a0194d0ba8222af9063e52850f6a822aef
Jonas Beyer <jonas.beyer@fau.de>
2019-12-06 10:15:38 +0100

    implemented bitmask behavior

--- Entity.h
+++ Entity.h
@@ -9,6 +9,22 @@
 enum Destiny {consumed_at_start, consumed_at_end, occupied, freed};
 enum Race {Protoss, Zerg, Terran};
 
+//turns a bitmask into a vector of those indices of the mask, where the bit is one
+//the mask is a template argument and the function is cached 
+template<int bitmask>
+inline std::vector<int>& mask_to_vector(){
+    static bool already_calculated = false;
+    static std::vector<int> v;
+    if(!already_calculated){
+        already_calculated = true;
+        int mask = bitmask;
+        for(size_t i = 0;i < 64;++i){
+            if(mask%2 == 1)v.push_back(i);
+            mask = mask>>1;
+        }
+    }
+    return v;
+}
 
 class AbstractEntity{
 protected:
@@ -81,7 +97,7 @@ class requirementNotFulfilledException : public std::exception {
    }
 };
 
-template<Race race, int clss_id, int mins, int gs, int sppl, int sppl_p, long prd, Destiny prd_d, int req, int maxOcc, int bldtime>
+template<Race race, int clss_id, int mins, int gs, int sppl, int sppl_p, long prd_mask, Destiny prd_d, long req_mask, int maxOcc, int bldtime>
 class Entity : public AbstractEntity{
 private:
     static auto getCounter() -> unsigned int& {
@@ -111,13 +127,13 @@ public:
         return sppl_p;
     }
     int producer() const override{
-        return prd;
+        return prd_mask;
     }
     Destiny producer_destiny() const override{
         return prd_d;
     }
     int requirement() const override{
-        return req;
+        return req_mask;
     }
     std::string id() const override{
         std::stringstream ss;
@@ -128,18 +144,26 @@ public:
     }
 
     static ProductionEntry* check_and_build(std::map<int , std::vector<AbstractEntity*>*> &entities_done, unsigned int& minerals, unsigned int& gas, unsigned int& supply_used, unsigned int& supply){
-        if(minerals < mins) throw noMineralsException();
-        if(gas<  gs) throw noGasException();
+        if(minerals < mins)throw noMineralsException();
+        if(gas < gs) throw noGasException();
         if(sppl < supply-supply_used) throw noSupplyException();
-        //TODO implement bismask behavior
-        //TODO implement requirements
-        std::vector<AbstractEntity*> possible_producers = *(entities_done[prd]);
-        for(AbstractEntity* producer : possible_producers){
-            if(producer->check_and_occupy()){
-                //TODO: Kill producer if consume_at_start
-                AbstractEntity* producee = new Entity<race, clss_id, mins, gs, sppl, sppl_p, prd, prd_d, req, maxOcc, bldtime>();
-                ProductionEntry* e = new ProductionEntry(producee, producer, bldtime);
-                return e;
+        for(int req_id : mask_to_vector<req_mask>()){
+            if(!(entities_done[req_id]->empty()))goto req_fulfilled;
+        }
+        throw requirementNotFulfilledException();
+
+        req_fulfilled:
+        //possibly search for chronoboosted producers?
+        for(int prd_id : mask_to_vector<prd_mask>()){
+            std::vector<AbstractEntity*> possible_producers = *(entities_done[prd_id]);
+            for(std::vector<AbstractEntity*>::iterator it = possible_producers.begin();it != possible_producers.end();++it){
+                AbstractEntity* producer = *it;
+                if(producer->check_and_occupy()){
+                    if(prd_d == Destiny::consumed_at_start)possible_producers.erase(it);
+                    AbstractEntity* producee = new Entity<race, clss_id, mins, gs, sppl, sppl_p, prd_mask, prd_d, req_mask, maxOcc, bldtime>();
+                    ProductionEntry* e = new ProductionEntry(producee, producer, bldtime);
+                    return e;
+                }
             }
         }
         throw noProducerAvailableException();
