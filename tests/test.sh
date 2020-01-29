642bbdd0fd4fa2c81cfde16bd89b45145516d4b9
Thomas Röthenbacher <thomas.roethenbacher@fau.de>
2020-01-29 17:43:03 +0100

    Small improvements to tests

--- tests/test.sh
+++ tests/test.sh
@@ -19,19 +19,22 @@ fi
 
 race=$1 
 file_name="$2" 
-temp_file="$(mktemp)"
+json_output="$(mktemp)"
+server_answer="$(mktemp)"
 
-"$dir_name"/../forwardSim.sh "$race" "$file_name" | curl -X POST --data-urlencode forwardSimulationLog@- https://www10.cs.fau.de/advptSC2/forwardSimulation/validation --header "Content-Type:application/x-www-form-urlencoded" 2>/dev/null >"$temp_file"
+"$dir_name"/../forwardSim.sh "$race" "$file_name" > "$json_output"
+curl -X POST --data-urlencode forwardSimulationLog@"$json_output" https://www10.cs.fau.de/advptSC2/forwardSimulation/validation --header "Content-Type:application/x-www-form-urlencoded" 2>/dev/null >"$server_answer"
 
-if grep '<div class="bs-callout bs-callout-danger">' "$temp_file" >/dev/null; then
-
-	echo "Doesn't seem to work, see" "$dir_name"/error.html "for the error message."
-    mv "$temp_file" "$dir_name"/error.html
+if grep '<div class="bs-callout bs-callout-danger">' "$server_answer" >/dev/null || grep 'Internal Server Error' "$server_answer" >/dev/null
+then
+	echo "Doesn't seem to work, see" "$dir_name"/error.html "for the error message and "$dir_name"/error.json for our output."
+    mv "$json_output" "$dir_name"/error.json
+    mv "$server_answer" "$dir_name"/error.html
 	exit 1
 
 else
-
-	rm "$temp_file"
+    rm "$json_output"
+    rm "$server_answer"
 	exit 0
 
 fi
